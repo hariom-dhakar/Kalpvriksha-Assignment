8e98592eeda021c87fdac7dc5d4a2ec071ae4a8c
unknown <harudhakar@gmail.com>
2025-10-09 14:18:58 +0530

    Enhanced the Update and Delete operation

--- crud_operation.c
+++ crud_operation.c
@@ -4,7 +4,6 @@
 
 #define FILENAME "users_data.txt"
 
-// User structure
 typedef struct
 {
      int id;
@@ -13,8 +12,8 @@ typedef struct
      int experience;
 } user;
 
-// Add user Operation
-void add_user(){
+void add_user()
+{
      FILE *fp = fopen(FILENAME, "a");
      if (fp == NULL)
      {
@@ -35,8 +34,8 @@ void add_user(){
      printf("New User added successfully\n");
 }
 
-// Show user operation
-void display_users(){
+void display_users()
+{
      FILE *fp = fopen(FILENAME, "r");
      if (fp == NULL)
      {
@@ -45,14 +44,15 @@ void display_users(){
      }
      user u;
      printf("ID\tName\tAge\tExperience\n");
-     while (fscanf(fp, "%d %s %d %d", &u.id, u.name, &u.age, &u.experience) == 4){
+     while (fscanf(fp, "%d %s %d %d", &u.id, u.name, &u.age, &u.experience) == 4)
+     {
           printf("%d\t%s\t%d\t%d\n", u.id, u.name, u.age, u.experience);
      }
      fclose(fp);
 }
 
-// Update user operation
-void update_user(){
+void update_user()
+{
      FILE *fp = fopen(FILENAME, "r");
      FILE *temp = fopen("temp.txt", "w");
      if (!fp || !temp)
@@ -64,34 +64,96 @@ void update_user(){
      int id, found = 0;
      printf("Enter the user ID to update: ");
      scanf("%d", &id);
+
+     user users[100];
+     int count = 0;
+     int total = 0;
+
      while (fscanf(fp, "%d %s %d %d", &u.id, u.name, &u.age, &u.experience) == 4)
      {
-          if (u.id == id)
-          {
-               printf("Enter the new Name: ");
-               scanf("%s", u.name);
-               printf("Enter the new Age: ");
-               scanf("%d", &u.age);
-               printf("Enter the new Experience: ");
-               scanf("%d", &u.experience);
-               found = 1;
-          }
-          fprintf(temp, "%d %s %d %d\n", u.id, u.name, u.age, u.experience);
+          users[total++] = u;
      }
      fclose(fp);
+
+     printf("User with User id: %d\n", id);
+     for (int i = 0; i < total; i++)
+     {
+          if (users[i].id == id)
+          {
+               count++;
+               printf("%d. Name: %s  Age: %d  Experience: %d\n",
+                      count, users[i].name, users[i].age, users[i].experience);
+          }
+     }
+
+     if (count == 0)
+     {
+          printf("No user found with this ID %d.\n", id);
+          fclose(temp);
+          remove("temp.txt");
+          return;
+     }
+
+     int choice;
+
+     if (count > 1)
+     {
+          printf("Multiple users found with same ID.\n");
+          printf("Enter the number (1-%d) of the user you want to update: ", count);
+          scanf("%d", &choice);
+          if (choice < 1 || choice > count)
+          {
+               printf("Invalid choice.\n");
+               fclose(temp);
+               remove("temp.txt");
+               return;
+          }
+     }
+     else
+     {
+          choice = 1;
+     }
+
+     int current = 0;
+     for (int i = 0; i < total; i++)
+     {
+          if (users[i].id == id)
+          {
+               current++;
+               if (current == choice)
+               {
+                    printf("Updating user %d (Name: %s, Age: %d, Exp: %d)\n",
+                           current, users[i].name, users[i].age, users[i].experience);
+                    printf("Enter the new Name: ");
+                    scanf("%s", users[i].name);
+                    printf("Enter the new Age: ");
+                    scanf("%d", &users[i].age);
+                    printf("Enter the new Experience: ");
+                    scanf("%d", &users[i].experience);
+                    found = 1;
+               }
+          }
+     }
+     for (int i = 0; i < total; i++)
+     {
+          fprintf(temp, "%d %s %d %d\n", users[i].id, users[i].name, users[i].age, users[i].experience);
+     }
      fclose(temp);
+
      remove(FILENAME);
      rename("temp.txt", FILENAME);
-     if (found){
+     if (found)
+     {
           printf("User updated successfully.\n");
      }
-     else{
-          printf("User not found.\n");
+     else
+     {
+          printf("User not found or update failed.\n");
      }
 }
 
-// Delete user operation
-void delete_user(){
+void delete_user()
+{
      FILE *fp = fopen(FILENAME, "r");
      FILE *temp = fopen("temp.txt", "w");
      if (!fp || !temp)
@@ -103,28 +165,75 @@ void delete_user(){
      int id, found = 0;
      printf("Enter the user ID to delete: ");
      scanf("%d", &id);
+     user users[100];
+     int total = 0;
+
      while (fscanf(fp, "%d %s %d %d", &u.id, u.name, &u.age, &u.experience) == 4)
      {
-          if (u.id == id){
-               found = 1;
-          }
-          else{
-               fprintf(temp, "%d %s %d %d\n", u.id, u.name, u.age, u.experience);
-          }
+          users[total++] = u;
      }
      fclose(fp);
+
+     printf("Users with user id: %d\n", id);
+     int count = 0;
+     for (int i = 0; i < total; i++)
+     {
+          if (users[i].id == id)
+          {
+               count++;
+               printf("%d. Name: %s  Age: %d  Experience: %d\n",
+                      count, users[i].name, users[i].age, users[i].experience);
+          }
+     }
+     if (count == 0)
+     {
+          printf("No user found with this User ID %d.\n", id);
+          return;
+     }
+     int choice = 1;
+     if (count > 1)
+     {
+          printf("Multiple users found with same User ID.\n");
+          printf("Enter the number (1-%d) of the user to delete: ", count);
+          scanf("%d", &choice);
+          if (choice < 1 || choice > count)
+          {
+               printf("Invalid choice.\n");
+               return;
+          }
+     }
+     int current = 0;
+     for (int i = 0; i < total; i++)
+     {
+          if (users[i].id == id)
+          {
+               current++;
+               if (current == choice)
+               {
+                    printf("Deleting user #%d (Name: %s, Age: %d, Exp: %d)\n",
+                           current, users[i].name, users[i].age, users[i].experience);
+                    found = 1;
+                    continue;
+               }
+          }
+          fprintf(temp, "%d %s %d %d\n",
+                  users[i].id, users[i].name, users[i].age, users[i].experience);
+     }
      fclose(temp);
      remove(FILENAME);
      rename("temp.txt", FILENAME);
-     if (found){
+     if (found)
+     {
           printf("User deleted successfully.\n");
      }
-     else{
+     else
+     {
           printf("User not found.\n");
      }
 }
 
-int main(){
+int main()
+{
      int choice;
      do
      {
